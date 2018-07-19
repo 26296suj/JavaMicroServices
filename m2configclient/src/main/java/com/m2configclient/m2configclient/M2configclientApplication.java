0e701b6c1eec67028f0a38d07690d50539a15135
Suj <spatel@flexerasoftware.com>
2018-07-19 15:58:35 +0100

    Added a client to read and present config values
    Using spring client to connect to our git hosted config server to pull and present remotely stored configuration.

---
+++ m2configclient/src/main/java/com/m2configclient/m2configclient/M2configclientApplication.java
@@ -0,0 +1,12 @@
+package com.m2configclient.m2configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2configclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2configclientApplication.class, args);
+	}
+}
---
+++ m2configclient/src/test/java/com/m2configclient/m2configclient/M2configclientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.m2configclient.m2configclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class M2configclientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
